fe58b2e0105a4cbe6429c24a0c964dd1c6a05af4
Eric Chaves <erchaves@gmail.com>
2015-12-03 09:16:29 -0500

    cleanup routes that don't need to connect

--- src/www/routes/RootRoute/About/Component.js
+++ src/www/routes/RootRoute/About/Component.js
@@ -1,31 +1,13 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import Markdown from '../../../components/Markdown';
-
-const propTypes = {
-  markdown: PropTypes.string,
-};
 
 class AboutRouteComponent extends Component {
   render() {
-    const { markdown } = this.props;
-
     return (
       <div className="container-fluid">
-        <div className="row">
-          <Markdown className="col-sm-12">
-            test about
-          </Markdown>
-        </div>
+        test about page
       </div>
     );
   }
 }
 
-AboutRouteComponent.propTypes = propTypes;
-
-const stateToProps = ({ markdown }) => ({
-  markdown,
-});
-
-export default connect(stateToProps)(AboutRouteComponent);
+export default AboutRouteComponent;
--- src/www/routes/RootRoute/Home/Component.js
+++ src/www/routes/RootRoute/Home/Component.js
@@ -1,42 +1,13 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import Textarea from '../../../components/Textarea';
-import updateMarkdown from '../../../actions/creators/updateMarkdown';
-import debounce from '../../../../utils/debounce';
-
-const propTypes = {
-  onTextareaChange: PropTypes.func,
-  textareaValue: PropTypes.string,
-};
 
 class HomeRouteComponent extends Component {
   render() {
-    const { onTextareaChange, textareaValue } = this.props;
-
     return (
       <div className="container-fluid">
-        <div className="row">
-          <Textarea
-            className="col-sm-12"
-            onChange={onTextareaChange}
-            value={textareaValue}
-            placeholder="Add some markdown code" />
-        </div>
+        Test home
       </div>
     );
   }
 }
 
-HomeRouteComponent.propTypes = propTypes;
-
-const stateToProps = ({ markdown }) => ({
-  textareaValue: markdown,
-});
-
-const dispatchToProps = dispatch => ({
-  onTextareaChange: debounce(function (e) {
-    dispatch(updateMarkdown(e.target.value));
-  }, 300),
-});
-
-export default connect(stateToProps, dispatchToProps)(HomeRouteComponent);
+export default HomeRouteComponent;
